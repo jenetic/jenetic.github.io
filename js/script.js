8c6bdb5e682086444da67f32667820bd8537f5f0
srnq <77878502+xzjenny@users.noreply.github.com>
2022-01-12 17:28:54 -0800

    Added projects to projects section.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Typewriter Effect
+function typeEffect(content, speed) {
+    let i = 0;
+    let text = content;
+    let interval = setInterval(function(){
+        document.getElementById("name").innerHTML += content.charAt(i);
+        i++;
+        if (i >= text.length) {
+            clearInterval(interval);
+        }
+    }, speed)
+}
+
+// Execute Functions
+typeEffect("Hi! I'm Jenny.", 120);
