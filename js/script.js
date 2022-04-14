5ad7823c289550ac0c4dd7f11b6770a3e25525cc
srnq <77878502+xzjenny@users.noreply.github.com>
2022-04-13 22:41:57 -0700

    Redesigned site

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 // Typewriter Effect
-function typeEffect(content, speed) {
-    let i = 0;
-    let text = content;
-    let interval = setInterval(function(){
-        document.getElementById("name").innerHTML += content.charAt(i);
-        i++;
-        if (i >= text.length) {
-            clearInterval(interval);
-        }
-    }, speed)
+const typeEffect = (content, speed) => {
+  let i = 0;
+  let text = content;
+  let interval = setInterval(function(){
+      document.getElementById("name").innerHTML += content.charAt(i);
+      i++;
+      if (i >= text.length) {
+          clearInterval(interval);
+      }
+  }, speed)
 }
-
-// Execute Functions
 typeEffect("Hi! I'm Jenny.", 120); 
