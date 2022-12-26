10d152f408faed861d73133f3e507db1c1fd2102
jenetic <jznnylam@gmail.com>
2022-12-26 00:09:29 -0800

    A bunch of minor updates

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,16 @@
 import './main.css';
 
-// declare global {
-//   interface Window {
-//     toggleColorScheme: any;
-//   }
-// }
+// Typewriter Effect
+const typeEffect = (content: string, speed: number): void => {
+  let i = 0;
+  let text = content;
+  let interval = setInterval(function(){
+      document.getElementById("title").innerHTML += content.charAt(i);
+      i++;
+      if (i >= text.length) {
+          clearInterval(interval);
+      }
+  }, speed)
+}
 
-// // Typewriter Effect
-// const typeEffect = (content: string, speed: number): void => {
-//   let i = 0;
-//   let text = content;
-//   let interval = setInterval(function(){
-//       document.getElementById("name").innerHTML += content.charAt(i);
-//       i++;
-//       if (i >= text.length) {
-//           clearInterval(interval);
-//       }
-//   }, speed)
-// }
-
-// // Dark mode toggle
-// const toggleColorScheme = (onload: boolean = false): void => {
-//   let body = document.body;
-//   let button = document.getElementById("toggle-color-scheme");
-
-//   if (onload) {
-//     if (localStorage.getItem("color-scheme") === null) {
-//       localStorage.setItem("color-scheme", "light");
-//       button.textContent = "Toggle Dark Mode";
-//     } else {
-//       if (localStorage.getItem("color-scheme") === "dark") {
-//         button.textContent = "Toggle Light Mode";
-//       }
-//     }
-//   } else {
-//     if (body.className === "dark") {
-//       localStorage.setItem("color-scheme", "light");
-//       button.textContent = "Toggle Dark Mode";
-//     } else {
-//       localStorage.setItem("color-scheme", "dark");
-//       button.textContent = "Toggle Light Mode";
-//     }
-//   }
-//   body.className = localStorage.getItem("color-scheme");
-// }
-
-// // Do stuff
-
-// typeEffect("Hi! I'm Jenny.", 120); 
-// toggleColorScheme(true);
+typeEffect("Hi! I'm Jenny.", 120);
